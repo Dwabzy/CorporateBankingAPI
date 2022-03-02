11aa2ab70baf50674d91d6fb928ee003ab1b40af
Dwabzy <jspragadeesh@gmail.com>
2022-03-02 13:01:51 +0530

    Added Starter Files

---
+++ src/main/java/com/virtusa/corporatebankingapi/CorporateBankingApiApplication.java
@@ -0,0 +1,13 @@
+package com.virtusa.corporatebankingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateBankingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporateBankingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/virtusa/corporatebankingapi/CorporateBankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtusa.corporatebankingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporateBankingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
