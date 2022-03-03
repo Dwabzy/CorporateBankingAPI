59265fcb1dabb7ad7a5a1dc75a88503ed1adb5bf
Velan-K <85840432+Velan-K369@users.noreply.github.com>
2022-03-03 13:30:40 +0530

    .

--- src/main/java/com/virtusa/corporatebankingapi/CorporateBankingApiApplication.java
+++ src/main/java/com/virtusa/corporatebankingapi/CorporateBankingApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CorporateBankingApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CorporateBankingApiApplication.class, args);
+		System.out.println("CorporateBanking App");
 	}
 
 }
