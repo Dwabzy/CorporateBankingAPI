bd59d3360f7a914a364fcd5e6f92236b81460d0e
nitinmano <55648552+nitinmano@users.noreply.github.com>
2022-03-03 19:40:35 +0530

    formatted... (#4)
    Signed-off-by: nitinmano <55648552+nitinmano@users.noreply.github.com>

--- src/main/java/com/virtusa/corporatebankingapi/customer/CustomerController.java
+++ src/main/java/com/virtusa/corporatebankingapi/customer/CustomerController.java
@@ -11,10 +11,10 @@ public class CustomerController {
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/customer")
-    public Customer customer(@RequestParam(value="salary", defaultValue = "20000") long salary,
-                             @RequestParam(value="customerName", defaultValue = "Bob") String customerName,
-                             @RequestParam(value="customerEmail", defaultValue = "bob@gmail.com") String customerEmail,
-                            @RequestParam(value="customerPanNumber", defaultValue = "1A2B3C") String customerPanNumber) {
+    public Customer customer(@RequestParam(value = "salary", defaultValue = "90") long salary,
+                             @RequestParam(value = "customerName", defaultValue = "") String customerName,
+                             @RequestParam(value = "customerEmail", defaultValue = "bob@gmail.com") String customerEmail,
+                             @RequestParam(value = "customerPanNumber", defaultValue = "1A2B3C") String customerPanNumber) {
         return new Customer(counter.incrementAndGet(), customerName, customerEmail, customerPanNumber, salary);
     }
 }
