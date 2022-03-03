3f5a6425d51a3781b89eed35dfd38657df2b8f74
Dwabzy <50489305+dwabzy@users.noreply.github.com>
2022-03-03 18:57:51 +0530

    Fixed Incorrect CustomerClass
    Signed-off-by: Dwabzy <jspragadeesh@gmail.com>

--- src/main/java/com/virtusa/corporatebankingapi/customer/Customer.java
+++ src/main/java/com/virtusa/corporatebankingapi/customer/Customer.java
@@ -3,10 +3,16 @@ package com.virtusa.corporatebankingapi.customer;
 public class Customer {
     private final long customerID;
     private final String customerName;
+    private final String customerEmail;
+    private final String customerPanNumber;
+    private final long salary;
 
-    public Customer(long customerID, String customerName) {
+    public Customer(long customerID, String customerName, String customerEmail, String customerPanNumber, long salary) {
         this.customerID = customerID;
         this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.customerPanNumber = customerPanNumber;
+        this.salary = salary;
     }
 
     public long getCustomerID() {
@@ -16,4 +22,18 @@ public class Customer {
     public String getCustomerName() {
         return customerName;
     }
+
+    public String getCustomerEmail(){
+        return customerEmail;
+    }
+
+    public String getCustomerPanNumber(){
+        return customerPanNumber;
+    }
+
+    public long getSalary(){
+        return salary;
+    }
+
+
 }
