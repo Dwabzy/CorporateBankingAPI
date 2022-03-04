27d8c3ba189e5b8b6796f86392c09a80d1415520
Dwabzy <50489305+dwabzy@users.noreply.github.com>
2022-03-04 14:14:06 +0530

    Added Database Connection + Customer Model
    Signed-off-by: Dwabzy <jspragadeesh@gmail.com>

--- src/main/java/com/virtusa/corporatebankingapi/customer/Customer.java
+++ src/main/java/com/virtusa/corporatebankingapi/customer/Customer.java
@@ -1,39 +1,111 @@
 package com.virtusa.corporatebankingapi.customer;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
 public class Customer {
-    private final long customerID;
-    private final String customerName;
-    private final String customerEmail;
-    private final String customerPanNumber;
-    private final long salary;
+    @Id
+    private String customerId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String address;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String dob;
 
-    public Customer(long customerID, String customerName, String customerEmail, String customerPanNumber, long salary) {
-        this.customerID = customerID;
-        this.customerName = customerName;
-        this.customerEmail = customerEmail;
-        this.customerPanNumber = customerPanNumber;
-        this.salary = salary;
+    public String getCustomerId() {
+        return customerId;
     }
 
-    public long getCustomerID() {
-        return customerID;
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
     }
 
-    public String getCustomerName() {
-        return customerName;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public String getCustomerEmail(){
-        return customerEmail;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public String getCustomerPanNumber(){
-        return customerPanNumber;
+    public String getLastName() {
+        return lastName;
     }
 
-    public long getSalary(){
-        return salary;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
+    public String getEmail() {
+        return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
 }
